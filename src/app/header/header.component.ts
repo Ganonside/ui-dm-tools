428a578e09cea4791ea5cd1d99d1a5f942a841e2
Jon Horner <hornerjn@mail.uc.edu>
2017-10-12 11:07:21 -0400

    Added Header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dm-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  title = 'DM Tools'
+}
