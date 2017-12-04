6276dfd13a7cd36f2ae2db35fdc3ba1351c53ad2
hornerjn <jon.horner@ingagepartners.com>
2017-12-04 16:12:42 -0500

    added combat component

---
+++ src/app/combat/combat.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-combat',
+  templateUrl: './combat.component.html',
+  styleUrls: ['./combat.component.scss']
+})
+export class CombatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addCharacter() {
+
+  }
+
+  startCombat() {
+
+  }
+}
---
+++ src/app/combat/combat.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CombatComponent } from './combat.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CombatComponent
+  ],
+  exports: [
+    CombatComponent
+  ]
+})
+export class CombatModule { }
